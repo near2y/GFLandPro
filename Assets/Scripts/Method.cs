40c67d88b0a8d32ad73fe8a6cd3d431c2b9b8c84
gaoxiangwu <near510@126.com>
2021-01-29 21:13:20 +0800

    完善scene03

---
+++ Assets/Scripts/Method.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Method
+{
+    /// <summary>
+    /// 判断是否在视野内
+    /// </summary>
+    /// <returns></returns>
+    public static bool InSight(Transform src,Transform tar, float angle)
+    {
+
+        Vector3 dir = tar.position - src.position;
+        return Vector3.Angle(src.forward, dir)<angle; 
+    }
+}
